bf922e71d40626ef714a13a99b077bebe3d169c6
kubealex <al.rossi87@gmail.com>
2022-01-03 23:30:32 +0100

    Added rabbitMQ sample

---
+++ rabbitMQ-interaction/producer.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	sock, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer sock.Close()
+
+	channel, err := sock.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer channel.Close()
+
+	queue, err := channel.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = channel.Publish(
+		"",
+		queue.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
